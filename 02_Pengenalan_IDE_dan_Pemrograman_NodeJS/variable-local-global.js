876b5cd7a1e3cc9d2f3b1483ef3c035cc822d375
TriMylani <trimylani5@gmail.com>
2025-02-24 12:17:28 +0700

    praktikum2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVariable = "Tri Mylani"
+
+function getMyIdentity() {
+    let localVariable = 2311104001
+    console.log (`Nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIdentity()
+
