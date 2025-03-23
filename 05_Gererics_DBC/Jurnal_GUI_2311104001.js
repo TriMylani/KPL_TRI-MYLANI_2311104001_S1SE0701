0cbe8b37acc77978d46655873423f9c2d8996499
TriMylani <trimylani5@gmail.com>
2025-03-23 17:54:30 +0700

    menambahkan class SimpleDataBase

--- 05_Gererics_DBC/Jurnal_GUI_2311104001.js
+++ 05_Gererics_DBC/Jurnal_GUI_2311104001.js
@@ -12,3 +12,30 @@ const angka3 = 10.0;
 console.log(
   `Hasil Penjumlahan: ${Penjumlahan.jumlahTigaAngka(angka1, angka2, angka3)}`
 );
+
+class SimpleDataBase {
+    constructor() {
+        this.storedData = [];
+        this.inputDates = [];
+    }
+
+    addNewData(data) {
+        this.storedData.push(data);
+        this.inputDates.push(new Date().toISOString());
+    }
+
+    printAllData() {
+        this.storedData.forEach((data, index) => {
+            console.log(`Data ${index + 1} berisi: ${data}, yang disimpan pada waktu UTC: ${this.inputDates[index]}`);
+        });
+    }
+}
+
+// Contoh penggunaan
+const db = new SimpleDataBase();
+db.addNewData(23);
+db.addNewData(11);
+db.addNewData(40);
+db.printAllData();
+
+module.exports = SimpleDataBase;
